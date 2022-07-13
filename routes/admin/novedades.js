dba2c72c04f67d62c6ccd330b507e561b70bc987
MartinGelvan <martingelvan@gmail.com>
2022-07-13 20:10:17 -0300

    Funcionamiento Guardar novedad y Eliminar novedad

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -18,4 +18,33 @@ router.get('/agregar',(req,res,next)=>{
     }) 
 })
 
+router.post('/agregar', async(req,res,next)=>{
+    // console.log(req.body);
+    try{
+        if(req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != ""){
+            await novedadesModel.insertNovedades(req.body);
+            res.redirect('/admin/novedades');
+        }else{
+            res.render('admin/agregar',{
+                layout:'admin/layout',
+                error:true,
+                message:'Todos los campos son requeridos'
+            })
+        }
+    }catch(error){
+        console.log(error);
+        res.render('admin/agregar',{
+            layout:'admin/layout',
+            error:true,
+            message:'No se carga la novedad'
+        })
+    }
+})
+
+router.get('/eliminar/:id', async(req,res,next)=>{
+    var id = req.params.id;
+    await novedadesModel.deleteNovedadById(id);
+    res.redirect('/admin/novedades');
+})
+
 module.exports = router;
